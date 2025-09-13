7a1d3af20016c6c5e4541d26e4bdbf0d7de665ac
Bomge69 <n-shabatura@mail.ru>
2025-09-13 14:25:57 +0300

    Домашняя работа 13 Наследование классов В классе BankAccount реализованы методы пополнения, списания и получения количества денег. В классе CardAccount унаследованного от класса BankAccount реализован метод списания денег с процентами. В классе DepositAccount унаследованного от класса BankAccount реализоввана логика списания денег только через месяц после пополнения счета.

--- src/main/java/practice/DepositAccount.java
+++ src/main/java/practice/DepositAccount.java
@@ -1,5 +1,23 @@
 package practice;
 
+import java.time.LocalDate;
+
 public class DepositAccount extends BankAccount {
 
+    //можно снимать деньги через месяц после последнего пополнения
+
+    LocalDate lastIncome;
+
+    @Override
+    public void put(double amountToPut) {
+        super.put(amountToPut);
+        lastIncome = LocalDate.now();
+    }
+
+    @Override
+    public void take(double amountToTake) {
+        if (LocalDate.now().isAfter(lastIncome.plusMonths(1))) {
+            super.take(amountToTake);
+        }
+    }
 }
